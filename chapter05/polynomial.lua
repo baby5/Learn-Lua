9ad754c1ce5fe803e349328f5f33229c902df601
baby5 <zxnzysj@163.com>
2018-09-05 01:15:47 +0800

    Update chapter05

--- chapter05/polynomial.lua
+++ chapter05/polynomial.lua
@@ -7,3 +7,18 @@ function poly(x, a_list)
     
     return r
 end
+
+
+function poly2(x, a_list)
+    local r = 0
+    
+    for i = #a_list, 2, -1 do
+        if i > 1 then
+            r = x * (a_list[i] + r)
+        else
+            r = r + a_list[i]
+        end
+    end
+    
+    return r
+end
