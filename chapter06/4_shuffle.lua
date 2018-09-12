38417d31ecbb597fb5740649de29d5cfb1b2ceb9
baby5 <zxnzysj@163.com>
2018-09-12 19:24:59 +0800

    Update chapter06

--- chapter06/4_shuffle.lua
+++ chapter06/4_shuffle.lua
@@ -1,11 +1,12 @@
 function shuffle(t)
     math.randomseed(os.time())
 
-    local size = #t
+    local size = #t 
     for i = size, 2, -1 do
         local j = math.random(i)
         t[i], t[j] = t[j], t[i]
     end
-
-    return t
+   
+   return t
 end
+
--- chapter06/6_call_chain.lua
+++ chapter06/6_call_chain.lua
@@ -1,3 +1,13 @@
-function func(i)
-    f = loadstring()
+n = math.random(1234567890)
+
+function func()
+    n = n - 1
+    if n == 0 then
+        return nil
+    else
+        return 'i = 1'
+    end
 end
+
+
+f = load(func)
