dec19b05e20a0576961e5b7d0c3c2d56a1962fc7
baby5 <zxnzysj@163.com>
2018-09-17 14:44:58 +0800

    Update

--- chapter05/list_insert.lua
+++
@@ -1,7 +0,0 @@
-function insert(l1, pos, l2)
-    for i = #l2, 1, -1 do
-        table.insert(l1, pos, l2[i])
-    end
-    
-    return l1
-end
--- chapter05/my_concat.lua
+++
@@ -1,8 +0,0 @@
-function my_concat(strs)
-    local s = ''
-    for _, str in ipairs(strs) do
-        s = s .. str
-    end
-
-    return s
-end
