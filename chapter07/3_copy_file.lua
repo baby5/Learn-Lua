0489fbd344fe0e7e05224d79deee4d019f273aa9
baby5 <zxnzysj@163.com>
2018-09-20 14:28:24 +0800

    Update chapter07

--- chapter07/3_copy_file.lua
+++ chapter07/3_copy_file.lua
@@ -1,27 +1,41 @@
 function copy(src, tag)
     io.input(src)
     io.output(tag)
-
+    
+    local start = os.time()
     while true do
         local char = io.read(1)
         if not char then break end
         io.write(char)
     end
+    io.stdout:write('byte by byte: ' .. os.time() - start .. '\n')
 
     io.input():seek('set', 0)
+    io.output(tag)
+    
+    local start = os.time()
     while true do
-        local line = io.read('l')
+        local line = io.read('*l')
         if not line then break end
         io.write(line)
     end
+    io.stdout:write('line by line: ' .. os.time() - start .. '\n')
 
     io.input():seek('set', 0)
+    io.output(tag)
+
+    local start = os.time()
     while true do
         local block = io.read(2^13)
         if not block then break end
-        io.write(line)
+        io.write(block)
     end
+    io.stdout:write('block by block: ' .. os.time() - start .. '\n')
 
     io.input():seek('set', 0)
-    io.write(io.read(a))
+    io.output(tag)
+
+    local start = os.time()
+    io.write(io.read('*a'))
+    io.stdout:write('whole: ' .. os.time() - start .. '\n')
 end
