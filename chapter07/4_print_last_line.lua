4a2896e0869b85e5321f37f6a4eb005fd24ea545
baby5 <zxnzysj@163.com>
2018-09-21 14:40:59 +0800

    update chapter07

---
+++ chapter07/4_print_last_line.lua
@@ -0,0 +1,27 @@
+function print_last(path)
+    local f = assert(io.open(path))
+    local size = f:seek('end')
+
+    multi_line = false
+    target = false
+    for i = size-1, 0, -1 do
+        f:seek('set', i)
+        local char = f:read(1)
+        
+        if char == '\n' then
+            if target then
+                print(f:read('*l'))
+                mulit_line = true
+                break
+            else
+                target = true
+            end
+        end
+    end
+    
+    -- only 1 line
+    if not mulit_line then
+        f:seek('set', 0)
+        print(f:read('*l'))
+    end
+end
