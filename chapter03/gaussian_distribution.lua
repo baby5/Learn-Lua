d6872e8f0bd11dc60ff62bc4faea2f1a38033670
baby5 <zxnzysj@163.com>
2019-06-30 13:54:14 +0800

    Fix syntax error

--- chapter03/gaussian_distribution.lua
+++ chapter03/gaussian_distribution.lua
@@ -1,9 +1,9 @@
-function clt():
+function clt()
     return math.random() + math.random()
 end
 
 
-function box_muller():
+function box_muller()
     u = math.random()
     v = math.random()
     return math.sqrt(-2 * math.log(u)) * math.sin(2 * math.pi * v)
